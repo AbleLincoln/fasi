3903206317a7f8dc9d6bffa24f9674fa7aca23f8
Andrew Maney <maneyandrew@gmail.com>
2022-01-09 22:00:00 -0600

    make post same layout as page

--- wp-content/themes/fasi/single.php
+++ wp-content/themes/fasi/single.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The single post page template.
  *
@@ -11,24 +12,13 @@ get_header();
 the_post();
 
 ?>
-
-	<main class="content container">
-		<div class="container">
-			<div class="row">
-				<div class="col-12">
-					<h1><?php the_title(); ?></h1>
-
-					<div class="entry">
-						<?php
-						defaultContent();
-						ContentBlock::display_theme_blocks();
-						wp_link_pages();
-						?>
-					</div>
-				</div>
-			</div>
-		</div>
-	</main>
-
+<main class="page-content">
+	<?php
+	get_theme_part('page/hero');
+	defaultContent();
+	ContentBlock::display_theme_blocks();
+	wp_link_pages();
+	?>
+</main>
 <?php
 get_footer();
