7e45dce97bd4e5adcb59f405a1797743790e0c11
AbleLincoln <maneyandrew@gmail.com>
2023-02-25 17:51:40 -0800

    will this make multiple?

--- wp-content/themes/fasi/parts/page/block-publications.php
+++ wp-content/themes/fasi/parts/page/block-publications.php
@@ -8,6 +8,13 @@
  * @since fasi 2.0
  */
 
+$fields = get_fields();
+function filter_for_years($key)
+{
+    return strpos($key, 'articles') === 0;
+}
+
+$years = array_filter($fields, 'filter_for_years', ARRAY_FILTER_USE_KEY);
 $articles = get_sub_field('articles_2023')
 ?>
 
@@ -21,31 +28,33 @@ $articles = get_sub_field('articles_2023')
             <!-- fetch all metadata from pubmed -->
             <!-- sort into years -->
             <!-- make accordion for each year -->
-            <div class="block-accordion__accordion-container col-12">
-                <div class="block-accordion__accordion">
-                    <div class="block-accordion__accordion-item">
-                        <div class="block-accordion__accordion-item-title">
-                            <button>2023</button>
-                        </div>
-                        <?php foreach ($articles as $article) { ?>
-                            <div class="block-accordion__accordion-item-content">
-                                <p>
-                                    <a href="<?php echo $article['link'] ?>">
-                                        <em><?php echo $article['title'] ?></em>
-                                    </a>
-                                </p>
-                                <small>
-                                    <?php echo $article['authors'] ?>
-                                </small>
-                                <br />
-                                <small>
-                                    <?php echo $article['citation'] ?>
-                                </small>
+            <?php foreach ($years as $year) { ?>
+                <div class="block-accordion__accordion-container col-12">
+                    <div class="block-accordion__accordion">
+                        <div class="block-accordion__accordion-item">
+                            <div class="block-accordion__accordion-item-title">
+                                <button>2023</button>
                             </div>
-                        <?php } ?>
+                            <?php foreach ($articles as $article) { ?>
+                                <div class="block-accordion__accordion-item-content">
+                                    <p>
+                                        <a href="<?php echo $article['link'] ?>">
+                                            <em><?php echo $article['title'] ?></em>
+                                        </a>
+                                    </p>
+                                    <small>
+                                        <?php echo $article['authors'] ?>
+                                    </small>
+                                    <br />
+                                    <small>
+                                        <?php echo $article['citation'] ?>
+                                    </small>
+                                </div>
+                            <?php } ?>
+                        </div>
                     </div>
                 </div>
-            </div>
+            <?php } ?>
         </div>
     </div>
 </section>
