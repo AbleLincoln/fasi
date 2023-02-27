041c18389e262d1d1885fc839642e84a4f3efe98
AbleLincoln <maneyandrew@gmail.com>
2023-02-27 00:33:36 -0800

    refactor pubs for future-proofed years

--- wp-content/themes/fasi/parts/page/block-publications.php
+++ wp-content/themes/fasi/parts/page/block-publications.php
@@ -8,8 +8,9 @@
  * @since fasi 2.0
  */
 
-$years = range(2023, 2017, -1);
-$articles = get_sub_field('articles_2023')
+// $years = range(2023, 2017, -1);
+// $articles = get_sub_field('articles_2023')
+$years = get_sub_field('publications_accordion');
 ?>
 
 <section class="block-accordion block-publications theme-light">
@@ -19,17 +20,14 @@ $articles = get_sub_field('articles_2023')
                 <h2 class="h3">Publications</h2>
             </div>
 
-            <!-- fetch all metadata from pubmed -->
-            <!-- sort into years -->
-            <!-- make accordion for each year -->
             <div class="block-accordion__accordion-container col-12">
                 <div class="block-accordion__accordion">
                     <?php foreach ($years as $year) { ?>
                         <div class="block-accordion__accordion-item">
                             <div class="block-accordion__accordion-item-title">
-                                <button><?php echo $year ?></button>
+                                <button><?php echo $year['year_name'] ?></button>
                             </div>
-                            <?php foreach (get_sub_field('articles_' . $year) as $article) { ?>
+                            <?php foreach ($year['articles'] as $article) { ?>
                                 <div class="block-accordion__accordion-item-content">
                                     <p>
                                         <a href="<?php echo $article['link'] ?>">
