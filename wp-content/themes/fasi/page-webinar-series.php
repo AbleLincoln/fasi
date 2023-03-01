b29ecf7c7ba3ac095b5343d37106583da6f6b62c
AbleLincoln <maneyandrew@gmail.com>
2023-03-01 12:49:38 -0800

    will this loop my posts?

--- wp-content/themes/fasi/page-webinar-series.php
+++ wp-content/themes/fasi/page-webinar-series.php
@@ -16,16 +16,31 @@ the_post();
     get_theme_part('page/hero');
     defaultContent();
     ContentBlock::display_theme_blocks();
-
-    get_posts(array("category" => "video"));
-
-    while (have_posts()) :
-        the_post();
-
-
     ?>
-        the post
-    <?php endwhile; ?>
+
+
+    <section class="block-content block-bottom-spacing" style="background-color:#001030;">
+        <div class="container">
+        </div>
+
+        <?php
+        $posts = get_posts(array("category" => "video"));
+
+        foreach ($posts as $post) {
+        ?>
+            <div class="row">
+                <div class="col-12 col-md-12 col-lg-10 offset-lg-1">
+                    <h4 style="color: #53d396; margin-top: 6rem; margin-bottom: 2rem;">Watch <span style="color: #ffffff;">Our Past Webinars</span></h4>
+                    <div style="padding:56.25% 0 0 0;position:relative;">
+                        <iframe src="https://player.vimeo.com/video/790953243?h=72eb8169de&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen="" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="January Webinar 2023.mp4" data-ready="true" frameborder="0"></iframe>
+                    </div>
+                </div>
+            </div>
+        <?php } ?>
+        <p>
+            <script src="https://player.vimeo.com/api/player.js"></script>
+        </p>
+    </section>
 
 </main>
 <?php
