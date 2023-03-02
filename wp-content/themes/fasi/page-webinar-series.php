4260d62b65ddebac622da26b9fbe280fd7d4a77e
AbleLincoln <maneyandrew@gmail.com>
2023-03-01 23:00:36 -0800

    ok going back into the webinar

--- wp-content/themes/fasi/page-webinar-series.php
+++ wp-content/themes/fasi/page-webinar-series.php
@@ -21,23 +21,24 @@ the_post();
 
     <section class="block-content block-bottom-spacing" style="display: none;background-color:#001030;">
         <div class="container">
-            <h4 style="color: #53d396; margin-top: 6rem; margin-bottom: 2rem;">Watch <span style="color: #ffffff;">Our Past Webinars</span></h4>
+            <div class="row">
+                <div class="col-12 col-md-12 col-lg-10 offset-lg-1">
+                    <h4 style="color: #53d396; margin-top: 6rem; margin-bottom: 2rem;">Watch <span style="color: #ffffff;">Our Past Webinars</span></h4>
 
-            <?php
-            $posts = get_posts(array("numberposts" => -1, "category" => 17)); // the 'webinar' cat
+                    <?php
+                    $posts = get_posts(array("numberposts" => -1, "category" => 17)); // the 'webinar' cat
 
-            foreach ($posts as $post) {
-                $id = $post->ID;
-                $video_iframe = get_field('video_thumbnail', $id);
-                $video_url = get_field('video_thumbnail', $id);
-                $thumbnail_url = get_the_post_thumbnail_url($post);
-            ?>
-                <div class="row">
-                    <div class="col-12 col-md-12 col-lg-10 offset-lg-1">
+                    foreach ($posts as $post) {
+                        $id = $post->ID;
+                        $video_iframe = get_field('video_thumbnail', $id);
+                        $video_url = get_field('video_thumbnail', $id);
+                        $thumbnail_url = get_the_post_thumbnail_url($post);
+                    ?>
                         <?php echo $video_iframe; ?>
-                    </div>
+                    <?php } ?>
                 </div>
-            <?php } ?>
+            </div>
+        </div>
         </div>
 
         <p>
